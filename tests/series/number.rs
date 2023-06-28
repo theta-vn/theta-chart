8248a04170446ef8cf1991e912adf2454e92a936
LangPham <phamxuanlang@gmail.com>
2023-06-28 11:27:34 +0700

    Add scatter chart

--- tests/series/number.rs
+++ tests/series/number.rs
@@ -9,16 +9,17 @@ fn new_series_number() {
 
 #[test]
 fn from_series_number() {
-    let vi64: Vec<i64> = vec![-36, 25, 10];
-    let linear = SNumber::from(vi64);
-    dbg!(&linear);
+    // let vi64: Vec<i64> = vec![-36, 25, 10];
+    // let linear = SNumber::from(vi64);
+    // dbg!(&linear);
 
-    let vu64: Vec<u64> = vec![36, 25, 10];
-    let linear = SNumber::from(vu64);
-    dbg!(&linear);
+    // let vu64: Vec<u64> = vec![36, 25, 10];
+    // let linear = SNumber::from(vu64);
+    // dbg!(&linear);
 
-    let vf64: Vec<f64> = vec![1., 2., 1.7];
-    let linear = SNumber::new(vf64);
+    let vf64: Vec<f64> = vec![1., 9., 1.7, 5.5, 3.5];
+    let linear = SNumber::new(vf64).set_range(0.5, 11.);
+    dbg!(&linear);
     let a = linear.gen_axes();
     dbg!(a);
 }
